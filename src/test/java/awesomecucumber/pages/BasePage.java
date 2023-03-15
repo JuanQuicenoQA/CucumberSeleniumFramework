27e302802082acd4fecb85a91f58d7dde6cc4844
Juan Quiceno <123400571+JuanQuicenoQA@users.noreply.github.com>
2023-03-15 15:37:38 -0500

    Update BasePage

--- src/test/java/awesomecucumber/pages/BasePage.java
+++ src/test/java/awesomecucumber/pages/BasePage.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 
 public class BasePage {
     public BasePage(WebDriver driver) {
-        this.driver = driver;
+        BasePage.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         PageFactory.initElements(driver, this);
     }
